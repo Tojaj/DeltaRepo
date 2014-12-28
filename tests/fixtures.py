e0c48496c89bbf5721931002765515b0c86911ef
Tomas Mlcoch <tmlcoch@redhat.com>
2014-12-28 20:18:32 +0100

    Tests: Update fixtures.py (Add new paths)

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -22,5 +22,9 @@ PACKAGE_ARCHER = os.path.join(PACKAGES_PATH, "Archer-3.4.5-6.x86_64.rpm")
 PACKAGE_RIMMER = os.path.join(PACKAGES_PATH, "Rimmer-1.0.2-2.x86_64.rpm")
 
 # testdata/repo_*
+REPO_00_PATH = os.path.join(TEST_DATA_PATH, "repo_00")
+REPO_00_PRIMARY = os.path.join(REPO_00_PATH, "repodata", "1cb61ea996355add02b1426ed4c1780ea75ce0c04c5d1107c025c3fbd7d8bcae-primary.xml.gz")
 REPO_01_PATH = os.path.join(TEST_DATA_PATH, "repo_01")
+REPO_01_PRIMARY = os.path.join(REPO_01_PATH, "repodata", "341297672077ef71a5f8db569932d20975e906f192986cdfa8ab535f0c224d4d-primary.xml.gz")
 REPO_02_PATH = os.path.join(TEST_DATA_PATH, "repo_02")
+REPO_02_PRIMARY = os.path.join(REPO_02_PATH, "repodata", "a7715505059733a63c49e66fffc7cf3aee6217ae05bede274a2b3e3e143de7c6-primary.xml.gz")
