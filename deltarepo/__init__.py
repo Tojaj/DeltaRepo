5aee94badb2046b77c1f290dad2cc985d650c5d9
Tomas Mlcoch <tmlcoch@redhat.com>
2014-12-28 13:44:50 +0100

    Add compression constants into deltarepo module's __init__.py

--- deltarepo/__init__.py
+++ deltarepo/__init__.py
@@ -39,3 +39,16 @@ __all__ = ['VERSION_MAJOR', 'VERSION_MINOR', 'VERSION_PATCH',
 
 VERSION = "{0}.{1}.{2}".format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 VERBOSE_VERSION = "%s (createrepo_c: %s)" % (VERSION, cr.VERSION)
+
+# Compression types
+AUTO_DETECT_COMPRESSION = cr.AUTO_DETECT_COMPRESSION    # Compression auto-detection
+UNKNOWN_COMPRESSION = cr.UNKNOWN_COMPRESSION            # Unknown compression type
+NO_COMPRESSION = cr.NO_COMPRESSION                      # No compression
+GZ_COMPRESSION = cr.GZ_COMPRESSION                      # Gzip compression
+BZ2_COMPRESSION = cr.BZ2                                # Bzip2 compression type
+XZ_COMPRESSION = cr.XZ                                  # Xz compression type
+
+# Compression types - shortcuts
+GZ = GZ_COMPRESSION                                     # Gzip compression type (shortcut)
+BZ2 = BZ2_COMPRESSION                                   # Bzip2 compression type (shortcut)
+XZ = XZ_COMPRESSION                                     # Xz compression type (shortcut)
